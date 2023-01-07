9372efb28687ba667a61e0f54fda02eedfee5b04
Vi <panagen3002@gmail.com>
2023-01-07 11:50:59 -0500

    moved "read_layout" to "layout_helper"

--- layout-helper.py
+++
@@ -1,5 +0,0 @@
-from read_layout import read_layout
-
-bottom_formatted = ''
-
-layout = read_layout("bottom.txt")
---
+++ layout_helper.py
@@ -0,0 +1,25 @@
+def read_layout(file):
+    file = open(f"./{file}", "r")
+
+    row = 0
+    layout = [[]]
+    for i in file:
+        if i != "\n":
+            layout[row].append(i.rstrip("\n"))
+        else:
+            row += 1
+            layout.append([])
+    return (layout)
+
+
+def format_layout(layout):
+    formatted_layout = ''
+    for i in layout:
+        for j in i:
+            formatted_layout += (f"[{j}]")
+        formatted_layout += "\n"
+    return (formatted_layout.rstrip("\n"))
+
+
+if __name__ == "__main__":
+    print(format_layout(read_layout("bottom.txt")))
