30ed6005895e438da2b23ffa8d9035b335bda50e
Vi <panagen3002@gmail.com>
2023-01-07 21:16:17 -0500

    work on making it into a pip package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="Layout Helper",
+      version="0.1",
+      description="shows the keyboard layout",
+      author="horse",
+      author_email="horsescary@gmail.com",
+      license="GPL3",
+      packages=['layout_helper'],
+      zip_safe=False,
+      scripts=["bin/layout_helper"])
